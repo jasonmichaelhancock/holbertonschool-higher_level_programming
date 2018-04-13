45ca44076194e227b6f9dadb62e4dbedda220fe5
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-04-13 03:56:07 +0000

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+c = len(sys.argv) - 1 
+if c == 0:
+    print("{:d} arguments.".format(c))
+elif c == 1:
+    print("{:d} argument:".format(c))
+    print("1: {}".format(str(sys.argv[1])))
+else:
+    print("{:d} arguments:".format(c))
+    for i in range(1, c + 1):
+        print("{}: {}".format(i, str(sys.argv[i])))
