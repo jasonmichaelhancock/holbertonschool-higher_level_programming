1042c4863161293878171b41ae3a4b5442cb4367
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-04-20 04:04:46 +0000

    task1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list
+    for i in range(0, len(new_list)):
+        if new_list[i] == search:
+            del new_list[i]
+            new_list.insert(i, replace)
+    return(new_list)
