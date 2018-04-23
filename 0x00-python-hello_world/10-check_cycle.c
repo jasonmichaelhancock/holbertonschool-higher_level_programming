c7dc48bfd2fbbe87934f174a50e20c2590c886ed
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-04-23 20:17:59 +0000

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * check_cycle - check for loop
+ * @list: the list.
+ *
+ * Return: Always 0 or 1.
+ */
+
+int check_cycle(listint_t *list)
+{
+listint_t *fast;
+listint_t *slow;
+
+fast = list;
+slow = list;
+while (fast != NULL && slow != NULL)
+{
+fast = fast->next->next;
+slow = slow->next;
+if (fast == slow)
+return(1);
+}
+return(0);
+}
