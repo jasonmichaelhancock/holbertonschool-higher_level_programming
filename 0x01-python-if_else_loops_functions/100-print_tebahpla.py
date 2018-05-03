f89fd4debe8e1e441d70b6afd9f6d2a7b4823d3e
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-03 04:07:25 +0000

    task14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+list = list(range(97, 123)
+for (i in list):
+    if (i % 2 == 0):
+        i = (i - 32)
+"""
+for letter in "zYxWvUtSrQpOnMlKjIhGfEdCbA":
+    print('{:s}'.format(letter), end = "")
