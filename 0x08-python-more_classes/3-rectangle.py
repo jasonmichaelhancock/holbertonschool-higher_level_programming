fe8b917360a34dab30247544c6f9ebb8b57e19bb
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-10 00:40:28 +0000

    task3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -41,4 +41,5 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return("")
         else:
-            return ((("#" * self.__width) + ('\n')) * (self.__height - 1)) + ("#" * self.__width)
+            return ((("#" * self.__width) + ('\n')) * (self.__height - 1)) \
+                   + ("#" * self.__width)
