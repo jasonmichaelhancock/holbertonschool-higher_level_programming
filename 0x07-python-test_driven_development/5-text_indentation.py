ed5943f6a7cff188e91ac0d5ab5ef9cebf2d18ac
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-15 01:53:27 +0000

    task4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 def text_indentation(text):
     i = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     for i in range(len(text)):
-        if (text[i - 1] == "." or text[i - 1] == "?" or text[i - 1] == ":") and text[i] == " ":
-            continue
-        elif text[i] == "." or text[i] == "?" or text[i] == ":":
-            print("{}".format(text[i]))
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print("{}\n".format(text[i]))
+            i += 1
+            while text[i] == " ":
+                i += 1
+
+        elif text[i] == " " and (text[i - 1] == "." or text[i - 1] == "?" or text[i - 1] == ":"):
+            while text[i] == " ":
+                i = i + 1
         else:
             print("{}".format(text[i]), end = "")
-        i += 1
+            i += 1
