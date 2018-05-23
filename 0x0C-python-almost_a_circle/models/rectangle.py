530a47b8414fdf6bb2801f10066106091db52d7c
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-23 04:58:05 +0000

    task2 corrections

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,6 +90,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """Function returns string rep of the display function."""
-        return ("[Rectangle] (" + str(self.id) + ") "\
-                + str(self.x) + "/" + str(self.y) + " - "\
+        return ("[Rectangle] (" + str(self.id) + ") " \
+                + str(self.x) + "/" + str(self.y) + " - " \
                 + str(self.width) + "/" + str(self.height))
