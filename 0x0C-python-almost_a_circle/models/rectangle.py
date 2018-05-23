1ca91ff288770ce1c6da1ebdabd4cf4a0ab55986
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-23 05:30:03 +0000

    task2 corrections

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,6 +88,7 @@ class Rectangle(Base):
         for i in range(self.__y):
             print()
         for i in range(self.__height):
+            print(" " * self.__x, end="")
             print("#" * self.__width)
 
     def __str__(self):
