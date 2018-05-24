e5ed1a5642813582b2cb20abafd438d762cfa7a5
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-24 06:38:05 +0000

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+
+class BaseGeometry:
+    
+    def area(self):
+        """function finds the area"""
+        raise Exception("area() is not implemented")
+
