096c68ff59e1167bdb3bcd79fcc817f9dc95980b
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-24 06:16:16 +0000

    task5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-    if issubclass(type(obj), a_class) and obj is not a_class:
+    """Return True if instance"""
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+
+class BaseGeometry:
+    pass
