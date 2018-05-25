50964020e935943590f7773e700b45cddac9ce6e
jasonmichaelhancock <34435212+jasonmichaelhancock@users.noreply.github.com>
2018-05-25 13:41:28 -0700

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,7 +10,8 @@ class Rectangle(BaseGeometry):
     
     def __init__(self, width, height):
         """Creates a new square"""
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width):
+        self.__width = width
+        self.integer_validator("height", height):
+        self.__height = height
+            
