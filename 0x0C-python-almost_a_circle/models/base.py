0cc07dada1659eff80ad42bc8ddf3b0aa035923d
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-26 01:06:46 +0000

    task14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module defines base class.
 """
+import json
 
 
 class Base:
@@ -17,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ """
+        if list_dictionaries is None:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
