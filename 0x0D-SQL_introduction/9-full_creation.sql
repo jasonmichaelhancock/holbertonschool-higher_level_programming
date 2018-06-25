65ec16e7351b0aba3ef600cd1e93b4599baa46a5
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-06-25 22:01:22 +0000

    tasks9,10

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Script creates a new table in SQL database.
+CREATE TABLE second_table (id INT NOT NULL, 
+name VARCHAR(256),
+score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
