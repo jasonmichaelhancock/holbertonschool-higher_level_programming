b062306466a7ce850f803b57b89f77fa0f33cfe5
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-06-25 20:48:57 +0000

    tasks6,7

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Script lists rows of a table.
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Script inserts a new line ito the table.
+INSERT INTO `first_table` (`id`,`name`) VALUES (89, "Holberton School");
