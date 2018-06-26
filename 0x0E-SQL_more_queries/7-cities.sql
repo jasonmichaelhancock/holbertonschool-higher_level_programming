ff687637ac3fef37929036eb1d55c9d36c2360f2
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-06-26 23:01:08 +0000

    task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- Create a database but do not fail if already exists, then add a user.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+state_id INT NOT NULL FOREIGN KEY(ID) REFERENCES states(id),
+name VARCHAR(256) NOT NULL);
