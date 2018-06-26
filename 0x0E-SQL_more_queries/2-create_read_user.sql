4c32a3ea2063f61b4b01b448ab5ffdf3f8a3bf9c
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-06-26 22:06:00 +0000

    task2 revised

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create a database but do not fail if already exists, then add a user.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
