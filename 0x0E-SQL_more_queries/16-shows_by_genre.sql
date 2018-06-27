2a2dbf0cf655babc5758ce2322cc20e148c619ff
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-06-27 05:23:44 +0000

    task16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- Script lists tv shows that have genres listed.
+
+SELECT tv_shows.title AS title, tv_genres.name AS name FROM tv_show_genres INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, name;
