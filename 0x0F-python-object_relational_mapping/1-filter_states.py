dbbb31114b4de56f36e0ab3ee53fd1974b77f2f1
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-20 21:53:49 +0000

    task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) > 3:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        query_rows = cursor.fetchall()
+        for row in query_rows:
+            print(row)
+        cursor.close()
+        db.close()
