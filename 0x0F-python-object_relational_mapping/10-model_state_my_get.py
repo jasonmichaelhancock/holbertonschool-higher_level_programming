00f5802b49a8e3ddaeb031be32c27a28ddb91216
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-21 05:47:23 +0000

    task10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True, connect_args= dict(port=3306))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    input = session.query(State).filter(State.name == argv[4]).all()
+    if (input == None):
+        print("None Found")
+    for input_id in input:
+        print("{}".format(input_id.id))
+    session.close()
