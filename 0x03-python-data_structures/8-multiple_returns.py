5043e894a8d9ce9e61953ce5763a77afdc01392f
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-29 23:55:40 +0000

    task8 revise or

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = ()
-    if sentence == None || sentence == "":
+    if sentence == None or sentence == "":
         a = (0, None)
     else:
         b = len(sentence)
