fb47d3e5808af5dd6dfc44895535c1ea5fd5c446
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-29 05:56:43 +0000

    task5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that prints My number: <first argument converted in integer>
+// if the first argument can be converted to an integer.
+let num = Number(process.argv[2]);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', num);
+}
