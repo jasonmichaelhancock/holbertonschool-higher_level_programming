886f453d53e5cec5f0856640a47479058dcd3ccb
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-29 07:03:23 +0000

    task10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 // A script that computes and prints a factorial.
 let num = Number(process.argv[2]);
-var factorial = function(num) {
-  if(num == 0) {
-    return 1
-  } else {
-    return num * factorial(num - 1);
+function factorial(num) {
+  if (isNaN(num)) {
+    return 1;
   }
+  if (Number(num) === 1) {
+    return 1;
+  }
+  return num * factorial(Number(num) - 1);
 }
 console.log(factorial(num));
