a8cee5bffddfbe5fc99813c4379382095dce9819
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-09-05 05:33:03 +0000

    task1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// An class Rectangle that defines a rectangle with w and h parameters.
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
