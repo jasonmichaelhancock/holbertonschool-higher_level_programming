c893c5e53d7905ddd3154dec49806c9360ce99cf
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-03-26 03:12:31 +0000

    add init

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+    '''
+    class inherits from list
+    '''
+
 class MyList(list):
     '''
     class inherits from list
     '''
+    def __init__(self):
+        '''
+        initializes new MyList object
+        '''
+        pass
+
     def print_sorted(self):
         '''
         print a sorted list
