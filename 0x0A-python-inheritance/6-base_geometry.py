fc91d5a2dcc14c5adf75c4bcd506c2ffd15df2ab
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-03-26 03:52:52 +0000

    docstring

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,8 +3,8 @@
 
 
 class BaseGeometry:
-    
+    """ defines class BaseGeometry """
+
     def area(self):
         """function finds the area the rectangle"""
         raise Exception("area() is not implemented")
-        
