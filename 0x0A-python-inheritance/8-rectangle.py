3ffc93637b88428e9426c7782a67d4d7c6e9f568
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-03-26 04:16:10 +0000

    init rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,16 +2,16 @@
 """
 Module documentation.
 """
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Class for strange squares"""
-    
+
     def __init__(self, width, height):
         """Creates a new square"""
-        self.integer_validator("width", width):
-        self.__width = width
-        self.integer_validator("height", height):
-        self.__height = height
-            
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
