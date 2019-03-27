d5207a5c778fb546eac3d6e94b145dff40bc5c56
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-03-27 04:54:35 +0000

    task1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Python script that fetches https://intranet.hbtn.io/status.'''
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(urllib.request.Request(argv[1])) as response:
+        print(response.headers.get('X-Request-Id'))
