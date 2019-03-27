b8ba4e8b4c10324e89c624a3f0ef26453091a84e
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-03-27 04:44:05 +0000

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
 
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(str(html, 'utf-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(str(html, 'utf-8')))
