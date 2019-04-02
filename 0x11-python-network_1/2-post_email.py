1941cc66cf421e0f4d9c54235eb750442dd7df02
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-04-02 05:09:13 +0000

    pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    data = {'email' : argv[2]}
+    data = {'email': argv[2]}
     data = parse.urlencode(data)
     data = data.encode('ascii')
     req = request.Request(url, data)
